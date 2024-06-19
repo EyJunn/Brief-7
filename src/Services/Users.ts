66512b40bf8b3cd69355bf888d419c5d40817894
Ey_jun <gregoiresenechal@outlook.com>
2024-06-19 09:58:52 +0200

    suppression d'une routes non utilisées

--- src/Services/Users.ts
+++ src/Services/Users.ts
@@ -27,28 +27,3 @@ export default function getMyAsset() {
 
 }
 
-export async function getAllAssets() {
-  let url = `${process.env.NEXT_PUBLIC_API_URL}user/users-assets`  
-  let jwt = window.localStorage.getItem("token")
-
-  let axiosConfig = {
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded;charset=utf-8',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-      Authorization: `Bearer ${jwt}`
-    },
-  }
-  try {
-    const res = await axios
-      .get(
-        url,
-        axiosConfig
-      );
-    return res.data;
-  } catch (e: any) {
-    console.log(e);
-    throw new Error(e);
-  }
-
-}
