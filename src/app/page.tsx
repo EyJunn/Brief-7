38c64bd190033aaf368d12f91569a9d492ee4478
Ey_jun <gregoiresenechal@outlook.com>
2024-06-20 10:23:32 +0200

    Mise en place d'un hover sur les liens de la première pages.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-tr from-violet-500 to-orange-300">
       <img src="KeeptoDream.png" />
-      <div className=" border-4 border-double border-white w-96 h-32">
+      <div className=" border-4 border-double border-white w-96 h-36 ">
         <h2 className="text-center text-2xl">Welcome on Keepto Dream. </h2>
 
-        <p className="text-center"> Here you can exchange your currency for crypto currency as well as exchange your crypto currencies with other users. So join us <a href="/register">here</a>  or log in <a href="/login">here</a></p>
+        <p className="text-center"> Here you can exchange your currency for crypto currency as well as exchange your crypto currencies with other users. So join us <a href="/register" className="hover:text-black pointer">here</a>  or log in <a href="/login" className="hover:text-black pointer">here</a></p>
       </div>
 
     </main>
