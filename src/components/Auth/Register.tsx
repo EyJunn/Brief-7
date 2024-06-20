970ead0d87ce0216b419c5ad773b889733c3878a
Ey_jun <gregoiresenechal@outlook.com>
2024-06-20 14:50:35 +0200

    mise a jour du register...

--- src/components/Auth/Register.tsx
+++ src/components/Auth/Register.tsx
@@ -1,31 +1,32 @@
 'use client'
 
-import {  registerUser } from '@/Services/auth'
+import { registerUser } from '@/Services/auth'
 import { useRouter } from 'next/navigation'
-import React  from 'react'
+import React from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { ErrorMsg } from '../Error'
-
 import { yupResolver } from '@hookform/resolvers/yup'
-import  yup from "yup"
+import yup from "yup"
 import { schema } from '@/validation/validator'
 import { registerProps } from '@/Utils/types'
+import { toast } from 'react-toastify'
 
 export const RegisterForm = () => {
   const { push } = useRouter()
 
- const {register, handleSubmit, watch, formState:{errors}}=useForm<registerProps>({mode:"onChange", resolver:yupResolver(schema)})
+  const { register, handleSubmit, watch, formState: { errors } } = useForm<registerProps>({ mode: "onChange", resolver: yupResolver(schema) })
 
   const onSubmit: SubmitHandler<registerProps> = (data) =>
     registerUser(data).then((res) => {
-if (res.status === 201){
-  if (typeof window !== 'undefined'){
-    window.localStorage.setItem('token', res.data.access_token)
-    window.localStorage.setItem('role', res.data.user.Role.name)
-    push('/Home')
-  }
-}
-    }).catch((e)=> console.log(e))
+      if (res.status === 201) {
+        if (typeof window !== 'undefined') {
+          toast.success("Welcome on Keepto Dream")
+          window.localStorage.setItem('token', res.data.access_token)
+          window.localStorage.setItem('role', res.data.user.Role.name)
+          push('/Home')
+        }
+      }
+    }).catch((e) => console.log(e))
 
   return (
     <div className="flex flex-col justify-center h-screen bg-inherit">
@@ -53,7 +54,7 @@ if (res.status === 201){
                 {...register('email')}
               />
             </div>
-            {errors.email  && <ErrorMsg message= {errors.email.message}/>}
+            {errors.email && <ErrorMsg message={errors.email.message} />}
           </div>
           <div>
             <div className="flex items-center justify-between">
@@ -72,7 +73,7 @@ if (res.status === 201){
                 {...register('firstName')}
               />
             </div>
-            {errors.firstName  && <ErrorMsg message={errors.firstName.message} />}
+            {errors.firstName && <ErrorMsg message={errors.firstName.message} />}
           </div>
           <div>
             <div className="flex items-center justify-between">
@@ -89,9 +90,9 @@ if (res.status === 201){
                 type="text"
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 indent-3"
                 {...register('lastName', { required: true })}
-              /> 
+              />
             </div>
-            {errors.lastName  && <ErrorMsg message={errors.lastName.message}/>}
+            {errors.lastName && <ErrorMsg message={errors.lastName.message} />}
           </div>
 
           <div>
@@ -109,9 +110,9 @@ if (res.status === 201){
                 type="number"
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 indent-3"
                 {...register('age', { required: true })}
-              /> 
+              />
             </div>
-            {errors.age  && <ErrorMsg message={errors.age.message}/>}
+            {errors.age && <ErrorMsg message={errors.age.message} />}
           </div>
 
           <div>
@@ -129,9 +130,9 @@ if (res.status === 201){
                 type="text"
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 indent-3"
                 {...register('pseudo', { required: true })}
-              /> 
+              />
             </div>
-            {errors.pseudo  && <ErrorMsg message={errors.pseudo.message}/>}
+            {errors.pseudo && <ErrorMsg message={errors.pseudo.message} />}
           </div>
 
           <div>
@@ -149,9 +150,9 @@ if (res.status === 201){
                 type="text"
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 indent-3"
                 {...register('city', { required: true })}
-              /> 
+              />
             </div>
-            {errors.city  && <ErrorMsg message={errors.city.message}/>}
+            {errors.city && <ErrorMsg message={errors.city.message} />}
           </div>
 
           <div>
@@ -172,10 +173,10 @@ if (res.status === 201){
                 {...register('password')}
               />
             </div>
-            {errors.password  && <ErrorMsg message={errors.password.message}/>}
+            {errors.password && <ErrorMsg message={errors.password.message} />}
           </div>
-          
-           <div>
+
+          <div>
             <div className="flex items-center justify-between">
               <label
                 htmlFor="promoCode"
@@ -191,32 +192,32 @@ if (res.status === 201){
                 autoComplete="current-promoCode"
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 indent-3 mb-4"
               />
-           </div> 
+            </div>
 
 
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Sign up
-            </button>
-          </div>
-        </div>
-       </form>
-        <div className="flex items-center justify-center space-x-2">
-              <span className="h-px w-16 bg-gray-300"></span>
-              <span className=" font-normal">OR</span>
-              <span className="h-px w-16 bg-gray-300"></span>
-         </div>
-          <div>
-              <a
-                className="w-full flex justify-center bg-black bg-opacity-50 text-gray-100 p-4 rounded-full tracking-wide font-semibold focus:outline-none focus:shadow-outline hover:bg-indigo-600 shadow-lg cursor-pointer transition ease-in duration-300"
-                href="/login"
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Log to your account
-              </a>
+                Sign up
+              </button>
+            </div>
           </div>
+        </form>
+        <div className="flex items-center justify-center space-x-2">
+          <span className="h-px w-16 bg-gray-300"></span>
+          <span className=" font-normal">OR</span>
+          <span className="h-px w-16 bg-gray-300"></span>
+        </div>
+        <div>
+          <a
+            className="w-full flex justify-center bg-black bg-opacity-50 text-gray-100 p-4 rounded-full tracking-wide font-semibold focus:outline-none focus:shadow-outline hover:bg-indigo-600 shadow-lg cursor-pointer transition ease-in duration-300"
+            href="/login"
+          >
+            Log to your account
+          </a>
+        </div>
       </div>
     </div>
   )
